9f95151be583376569124de6bebf521e649069e0
xbsir <nguyensophia085@gmail.com>
2025-02-21 00:39:30 -0500

    Django REST Framework

--- base/views.py
+++ base/views.py
@@ -180,3 +180,12 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
